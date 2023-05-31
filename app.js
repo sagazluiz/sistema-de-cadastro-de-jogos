a28ab12e64c0ac4707470d8425e11df88c602cb4
sagazluiz <sagazluiz@hotmail.com>
2023-05-31 00:14:07 -0300

    adicionando o console log no app.js

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos.")
 console.log(" Você tem várias formas de rodar esse programa.")
 // mais um console log de teste
 console.log("O Paulo fez esse commit sem você ver na aula!")
+console.log("Enviando para o Git hub")
